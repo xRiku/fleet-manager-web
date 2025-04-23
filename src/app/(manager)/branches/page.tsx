23fb2507ec71202559ea5f9c12d7de5910ca3864
Joao <joaocelso01@gmail.com>
2025-04-23 14:07:02 -0300

    feat: implement branch cards for improved display; add card component for branch details and vehicle availability

--- src/app/(manager)/branches/page.tsx
+++ src/app/(manager)/branches/page.tsx
@@ -1,19 +1,21 @@
 import BranchesModalTriggerButton from "@/components/buttons/branches-modal-trigger-button";
 import { BranchDialog } from "@/components/dialogs/branch-dialog";
 import { getBranches } from "@/lib/server-utils";
+import BranchCard from "@/components/branch-card";
 
 export default async function Page() {
   const branches = await getBranches();
-
   return (
     <div className="flex flex-col items-start justify-center">
       <div className="flex items-center justify-between w-full mt-8">
         <h1 className="text-xl font-semibold mb-6">Filiais</h1>
         <BranchesModalTriggerButton />
       </div>
-      <ul className="flex flex-col gap-2">
+      <ul className="grid grid-cols-[repeat(4,minmax(100px,500px))] gap-4">
         {branches.map((branch, index) => (
-          <li key={`${branch.name}-${index}`}>{branch.name}</li>
+          <BranchCard branch={branch} key={`${branch.name}-${index}`}/>
+
+          // <li key={`${branch.name}-${index}`}>{branch.name}</li>
         ))}
       </ul>
       <BranchDialog />
---
+++ src/components/branch-card.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { Branch, Vehicle } from "@/types";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Badge } from "./ui/badge";
+import { Garage } from "@phosphor-icons/react";
+export default function BranchCard({branch}: {branch: Branch}) {
+  const [availableVehicles, setAvailableVehicles] = useState<Vehicle[]>([]);
+  const [unavailableVehicles, setUnavailableVehicles] = useState<Vehicle[]>([]);
+  const [isLoadingVehicles, setIsLoadingVehicles] = useState(true);
+
+  useEffect(() => {
+      async function asyncFunction() {
+      try {
+        const fetchedVehicles = await fetch(`/api/vehicles/${branch.id}`);
+        const parsedFetchedVehicles = await fetchedVehicles.json();
+        setAvailableVehicles(parsedFetchedVehicles.filter((vehicle: Vehicle) => vehicle.availability === "available"));
+        setUnavailableVehicles(parsedFetchedVehicles.filter((vehicle: Vehicle) => vehicle.availability !== "available"));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoadingVehicles(false);
+      }
+    }
+    asyncFunction();
+  }, [branch]);
+
+
+  return (
+    <Card className="w-64 hover:shadow-lg transition">
+      <CardHeader className="flex items-center gap-2">
+        <Garage weight="bold" size={20}/>
+        <CardTitle>{branch.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <Badge variant="success" className="min-w-[180px] justify-between">Veículos disponíveis: <strong>{availableVehicles.length}</strong></Badge>
+        <Badge variant="destructive" className="min-w-[180px] justify-between">Veículos indisponíveis: <strong>{unavailableVehicles.length}</strong></Badge>
+      </CardContent>
+    </Card>
+  );
+
+}
